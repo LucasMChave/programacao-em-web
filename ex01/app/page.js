cbf41248d562bd72b10d8690b7f7794e5572e557
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-08-20 20:11:17 +0000

    linkedin funcional

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -4,8 +4,7 @@ export default function Home() {
       <h1>Lucas Mourato Aureliano de Melo</h1>
       <p>RA: 00000848725 | Estudante de C3 Unicap de 5º periodo</p>
 
-      <a href="www.linkedin.com/in/lucas-mourato-5b6a062b5"
-      target="blank_">
+      <a href="https://www.linkedin.com/in/lucas-mourato-5b6a062b5/">
         LinkedIn
       </a>
     </div>
