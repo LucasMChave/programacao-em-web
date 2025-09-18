866b8849a53bd0ae03eacbc6444e06334d61aef9
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-09-18 20:35:15 +0000

    teste audio2

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <audio src="/Assets/Bellhart OST EXTENDED - Hollow Knight Silksong.mp3" loop>teste</audio>
+      <audio src="/Assets/Bellhart-OST-EXTENDED-Hollow Knight Silksong.mp3" autoPlay loop muted controls>teste</audio>
       <h1>Lucas Mourato Aureliano de Melo</h1>
       <p>RA: 00000848725 | Estudante de C3 Unicap de 5º periodo | 22 anos de Idade</p>
       <img src="https://media.licdn.com/dms/image/v2/D4E03AQFYNW8K7zavWQ/profile-displayphoto-crop_800_800/B4EZjJN5aGHIAI-/0/1755722521089?e=1758758400&v=beta&t=fE9SdhydvdrHaPI36ect_FW1ts5kE8gR5Ze4sKgF38w"
