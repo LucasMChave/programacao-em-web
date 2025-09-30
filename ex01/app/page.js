e2f9f148bc8b34d012c14ba1ae9d4c2dc5084c7f
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-09-29 21:15:00 -0300

    testecor+pagina

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -4,7 +4,6 @@ import './maincss.css';
 
 export default function Home() {
   return (
-    backgroundColor = 'black',
     <div>
       <h1>Lucas Mourato Aureliano de Melo</h1>
       <p>RA: 00000848725 | Estudante de C3 Unicap de 5º periodo | 22 anos de Idade</p>
---
+++ ex01/app/teste/page.js
@@ -0,0 +1,9 @@
+import './maincss.css';
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Hello world</h1>
+        </div>
+  );
+}
