fea417f95921cc5da3c19053c669d06b49c9233b
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-09-29 22:02:26 -0300

    esqueciobodyloooooooool

--- ex01/app/jogo_da_forca/page.js
+++ ex01/app/jogo_da_forca/page.js
@@ -4,10 +4,12 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
-        <p>TBD</p>
-        <Image src="/Assets/hangedman.png" alt="TBD" width="250" height="250"></Image>
-        <Link href = "/">VOLTAR</Link>
-    </div>
+    <body>
+      <div>
+          <p>TBD</p>
+          <Image src="/Assets/hangedman.png" alt="TBD" width="250" height="250"></Image>
+          <Link href = "/">VOLTAR</Link>
+      </div>
+    </body>
   );
 }
