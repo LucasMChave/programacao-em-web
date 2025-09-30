0f55eb4a6fa4e23736ef36c21f61fe036064c4b0
LucasMouratoCH <132353766+LucasMChave@users.noreply.github.com>
2025-09-29 21:41:39 -0300

    forca?

--- ex01/app/jogo_da_forca/page.js
+++ ex01/app/jogo_da_forca/page.js
@@ -1,9 +1,11 @@
 import './forcacss.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-        <h1>Hello world</h1>
+        <p>TBD</p>
+        <Image src="/Assets/hangedman.jpg" alt="TBD" width="250" height="250"></Image>
         </div>
   );
 }
